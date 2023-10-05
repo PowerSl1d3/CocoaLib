ba45540581f29c9727cd957bf58aa30ff98bd23f
Олег Аксененко <aksenenko_oyu@drom.ru>
2023-10-05 21:52:43 +1000

    Initial commit

---
+++ Example/CocoaLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CocoaLib
+//
+//  Created by Олег Аксененко on 10/05/2023.
+//  Copyright (c) 2023 Олег Аксененко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
